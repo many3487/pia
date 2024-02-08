ab3327461c7bb10c6ef04f353d421a2baf97bb8e
many3487 <manuelja@hotmail.es>
2024-02-08 11:49:11 -0500

    lol

--- proyecto/app.py
+++ proyecto/app.py
@@ -11,6 +11,10 @@ user_model = ModelUser()
 def home():
     return render_template('cursos.html')
 
+@app.route('/icon-home')
+def icon_home():
+    return render_template('icon-home.html')
+
 @app.route('/menu')
 def menu():
     return render_template('menu.html')
